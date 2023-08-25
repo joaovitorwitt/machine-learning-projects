c670ffa2fd73a7570a2056f9645b43778f15db69
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-08-25 14:02:21 -0300

    Update main.py

--- basic-image-classification/main.py
+++ basic-image-classification/main.py
@@ -1,4 +1,7 @@
+
+
 """
+    Tutorial url: https://www.tensorflow.org/tutorials/keras/classification?hl=en
     train a model to classify images of clothing, like sneakers and shirts
 """
 
@@ -56,12 +59,67 @@ test_images = test_images / 255.0
     lets verify if the data is in correct format displaying the first 25 images with the class name below
 """
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+
+"""
+    Build the model
+    first we need to configure the layers
+"""
+
+"""
+    layers extract representations from the data
+    these representations must be meaningful for the problem
+"""
+model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+                             tf.keras.layers.Dense(128, activation='relu'),
+                             tf.keras.layers.Dense(10)])
+
+"""
+    the first layer transforms the format of the images from a two-dimensional array (28x28)
+    to a one-dimensional array (28*28 = 784)
+"""
+
+"""
+    Compile the model
+
+    before compiling the model we need a loss function, a optimizer, metrics
+
+    Loss Function = measures how accurate the model is during training
+    Optimizer = how the model is updated based on the data it sees and the loss function
+    Metrics = type of metrics used to monitor the training and testing steps
+"""
+
+model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
+
+
+"""
+    Train the model
+
+    1st - feed the training data into the model
+    2nd - the models learns to associate images and labels
+    3rd - ask the model to make predictions about a test set
+    4th - verify that the predictons match the labels from a test label
+"""
+
+model.fit(train_images, train_labels, epochs=10)
+
+"""
+    the model reaches about 90% accuracy on the training data
+"""
+
+"""
+    Next we need to evaluate the accuracy
+"""
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+print(f"\nTest accuracy: {test_acc}")
